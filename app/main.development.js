baba78c25fa4f1a1d7e316222d7ae17c00a65133
raulanatol <raul@natol.es>
2017-08-06 14:34:05 +0100

    Config autoupdater

--- app/main.development.js
+++ app/main.development.js
@@ -1,20 +1,22 @@
 import Database from "./desktop/Database";
 import Config from "./desktop/Config";
 import { getMenu } from "./desktop/utils/menu"
+import pkg from './package.json';
 import IPCEvents from "./desktop/IPCEvents";
+import { initAutoUpdater, checkForUpdates } from './desktop/utils/autoupdater';
 
-const { app, BrowserWindow, Menu, shell } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
-let mainWindow = null;
+let mainWindow: Electron.BrowserWindow = null;
 
 const config = new Config(app);
 const database = new Database(config);
 new IPCEvents(database, config);
 
-
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support'); // eslint-disable-line
   sourceMapSupport.install();
+  initAutoUpdater(mainWindow, pkg.version);
 }
 
 if (process.env.NODE_ENV === 'development') {
@@ -69,7 +71,8 @@ function onReady() {
   mainWindow.loadURL(`file://${__dirname}/app.html`);
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.show();
-    //   mainWindow.focus();
+    mainWindow.focus();
+    checkForUpdates();
   });
   mainWindow.on('closed', () => mainWindow = null);
   devTools();
